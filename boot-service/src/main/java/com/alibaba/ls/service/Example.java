1ae96c872c5b948211bb8d6a16614aafc7710429
lvsheng <lvsheng.ls@alibaba-inc.com>
2017-09-10 11:06:25 +0800

    eureka

--- boot-service/src/main/java/com/alibaba/ls/service/Example.java
+++ boot-service/src/main/java/com/alibaba/ls/service/Example.java
@@ -2,6 +2,8 @@ package com.alibaba.ls.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,15 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2017/05/22
  */
 
-@RestController
-@EnableAutoConfiguration
+@EnableEurekaServer
+@SpringBootApplication
 public class Example {
-
+	
 	@RequestMapping("/")
 	String home() {
-		return "Hello World!";
+		return "Hello 吕胜 !";
 	}
-
+	
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Example.class, args);
 	}
