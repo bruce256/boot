ccdf183268ee412401fd913de30608af3f650a1e
lvsheng <lvsheng.ls@alibaba-inc.com>
2018-01-28 23:40:24 +0800

    velocity

---
+++ boot-web/src/main/java/com/alibaba/ls/web/IndexController.java
@@ -0,0 +1,20 @@
+package com.alibaba.ls.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @auther 儒尊
+ * @date 2018/1/28
+ **/
+@Controller
+@RequestMapping("/index")
+public class IndexController {
+	
+	@RequestMapping("/")
+	public String index(ModelMap modelMap) {
+		modelMap.addAttribute("name", "ruzun");
+		return "index";
+	}
+}
