5855566df189325f38ccbd58b1ad3240a0970e9c
“bruce256” <326375225@qq.com>
2021-11-08 11:55:01 +0800

    获取applicationContext新方法

--- boot-web/src/main/java/com/alibaba/ls/web/controller/DemoController.java
+++ boot-web/src/main/java/com/alibaba/ls/web/controller/DemoController.java
@@ -2,6 +2,7 @@ package com.alibaba.ls.web.controller;
 
 import com.alibaba.ls.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,14 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 public class DemoController {
-	
-	@Autowired
-	private DemoService demoService;
-	
-	@RequestMapping("/home")
-	public String home() {
-		System.out.println(this);
-		demoService.hello();
-		return "Hello 吕胜 !";
-	}
+
+    @Autowired
+    private DemoService demoService;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @RequestMapping("/home")
+    public String home() {
+        System.out.println(applicationContext.getEnvironment());
+        System.out.println(this);
+        demoService.hello();
+        return "Hello 吕胜 !";
+    }
 }
