551ea483d15eb825fa57943c5a0bd011b91176c8
“bruce256” <326375225@qq.com>
2025-01-19 22:29:06 +0800

    接入Prometheus，pushgateway

--- boot-web/src/main/java/com/alibaba/ls/web/controller/RedisController.java
+++ boot-web/src/main/java/com/alibaba/ls/web/controller/RedisController.java
@@ -7,9 +7,6 @@ import com.alibaba.nacos.api.annotation.NacosInjected;
 import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.alibaba.nacos.api.naming.NamingService;
 
-import io.prometheus.metrics.core.metrics.Gauge;
-import io.prometheus.metrics.exporter.pushgateway.PushGateway;
-import io.prometheus.metrics.model.snapshots.Unit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,44 +25,30 @@ public class RedisController {
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
-    @NacosValue(value = "${owner:lvsheng}", autoRefreshed = true)
+/*    @NacosValue(value = "${owner:lvsheng}", autoRefreshed = true)
     private String owner;
 
     @NacosValue(value = "${number:1}", autoRefreshed = true)
-    private Integer number;
+    private Integer number;*/
 
-    @NacosInjected
-    private NamingService namingService;
-    private PushGateway pushGateway = PushGateway.builder()
-        .address("localhost:9091") // not needed as localhost:9091 is the default
-        .job("example")
-        .build();
+    //@NacosInjected
+    //private NamingService namingService;
+    //private PushGateway pushGateway = PushGateway.builder()
+    //    .address("localhost:9091") // not needed as localhost:9091 is the default
+    //    .job("example")
+    //    .build();
 
-    private Gauge dataProcessedInBytes = Gauge.builder()
-        .name("data_processed")
-        .help("data processed in the last batch job run")
-        .unit(Unit.BYTES)
-        .register();
+
 
     @GetMapping("/get")
     public String get(@RequestParam(value = "key") String key) {
         return stringRedisTemplate.boundValueOps(key).get();
     }
 
-    @GetMapping("/owner")
-    public String getOwner() {
-        dataProcessedInBytes.inc(1);
-        try {
-            pushGateway.push(dataProcessedInBytes);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return owner;
-    }
 
     @GetMapping("/number")
     public Integer getNumber() {
-        return number;
+        return null;
     }
 
 /*    @GetMapping("/name")
