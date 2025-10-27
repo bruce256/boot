dcc97f1b6fc11d0742a74eb3d283bfeef4796c53
“bruce256” <326375225@qq.com>
2025-10-27 22:22:05 +0800

    上报用户信息

---
+++ boot-web/src/main/java/com/alibaba/ls/web/SentryConfig.java
@@ -0,0 +1,22 @@
+package com.alibaba.ls.web;
+
+import org.springframework.stereotype.Component;
+import io.sentry.protocol.User;
+import io.sentry.spring.SentryUserProvider;
+
+/**
+ * @auther: LvSheng
+ * @date: 2025/10/27
+ * @description:
+ */
+@Component
+public class SentryConfig implements SentryUserProvider {
+	
+	public User provideUser() {
+		User user = new User();
+		user.setId("7570488");
+		user.setUsername("bruce");
+		// ... set user information
+		return user;
+	}
+}
